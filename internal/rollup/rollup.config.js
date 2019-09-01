969f9d483e4c3147394bacfeb23c28aadb7c9007
Aviv Stern <avivstern@outlook.com>
2019-08-31 22:16:00 -0700

    no message

--- internal/rollup/rollup.config.js
+++ internal/rollup/rollup.config.js
@@ -171,7 +171,8 @@ const config = {
     // Always fail on warnings, assuming we don't know which are harmless.
     // We can add exclusions here based on warning.code, if we discover some
     // types of warning should always be ignored under bazel.
-    throw new Error(warning.message);
+    console.log(warning.message);
+    // throw new Error(warning.message);
   },
   plugins: [TMPL_additional_plugins].concat([
     {
